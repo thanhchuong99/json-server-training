295c195c693544a0fa2259a3ff3e8057f602cf60
thanhchuong99 <nguyenthanhchuonghth@gmail.com>
2022-03-08 12:32:11 +0700

    fix jwt api

--- main.js
+++ main.js
@@ -5,6 +5,11 @@ const bodyParser = require("body-parser");
 const jwt = require("jsonwebtoken");
 
 const server = jsonServer.create();
+server.use(bodyParser.urlencoded({ extended: true }));
+// To handle POST, PUT and PATCH you need to use a body-parser
+// You can use the one used by JSON Server
+server.use(bodyParser.json());
+server.use(jsonServer.bodyParser);
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const userdb = JSON.parse(fs.readFileSync("users.json", "UTF-8"));
@@ -16,63 +21,78 @@ server.use(middlewares);
 server.get("/echo", (req, res) => {
   res.jsonp(req.query);
 });
-// To handle POST, PUT and PATCH you need to use a body-parser
-// You can use the one used by JSON Server
-server.use(bodyParser.urlencoded({ extended: true }));
-server.use(bodyParser.json());
-server.use(jsonServer.bodyParser);
+
 //auth
 // expire
-const SECRET_KEY = "123456789";
-const expiresIn = "1h"; // Create a token from a payload
+const SECRET_KEY = "72676376";
+
+const expiresIn = "1h";
+
 function createToken(payload) {
   return jwt.sign(payload, SECRET_KEY, { expiresIn });
 }
 
-// Verify the token
-function verifyToken(token) {
-  return jwt.verify(token, SECRET_KEY, (err, decode) =>
-    decode !== undefined ? decode : err,
-  );
-}
-
-// Check if the user exists in database
-function isAuthenticated({ email, password }) {
+function isLoginAuthenticated({ email, password }) {
   return (
     userdb.users.findIndex(
       (user) => user.email === email && user.password === password,
     ) !== -1
   );
 }
-server.post("/auth/login", (req, res) => {
+
+function isRegisterAuthenticated({ email }) {
+  return userdb.users.findIndex((user) => user.email === email) !== -1;
+}
+
+server.post("/api/auth/register", (req, res) => {
   const { email, password } = req.body;
-  if (isAuthenticated({ email, password }) === false) {
+  if (isRegisterAuthenticated({ email })) {
     const status = 401;
-    const message = "Incorrect email or password";
+    const message = "Email already exist";
+    res.status(status).json({ status, message });
+    return;
+  }
+
+  fs.readFile("./users.json", (err, data) => {
+    if (err) {
+      const status = 401;
+      const message = err;
+      res.status(status).json({ status, message });
+      return;
+    }
+    data = JSON.parse(data.toString());
+
+    let last_item_id = data.users[data.users.length - 1].id;
+
+    data.users.push({ id: last_item_id + 1, email: email, password: password });
+    let writeData = fs.writeFile(
+      "./users.json",
+      JSON.stringify(data),
+      (err, result) => {
+        if (err) {
+          const status = 401;
+          const message = err;
+          res.status(status).json({ status, message });
+          return;
+        }
+      },
+    );
+  });
+  const access_token = createToken({ email, password });
+  res.status(200).json({ access_token });
+});
+
+server.post("/api/auth/login", (req, res) => {
+  const { email, password } = req.body;
+
+  if (!isLoginAuthenticated({ email, password })) {
+    const status = 401;
+    const message = "Incorrect Email or Password";
     res.status(status).json({ status, message });
     return;
   }
   const access_token = createToken({ email, password });
   res.status(200).json({ access_token });
-});
-server.use(/^(?!\/auth).*$/, (req, res, next) => {
-  if (
-    req.headers.authorization === undefined ||
-    req.headers.authorization.split(" ")[0] !== "Bearer"
-  ) {
-    const status = 401;
-    const message = "Bad authorization header";
-    res.status(status).json({ status, message });
-    return;
-  }
-  try {
-    verifyToken(req.headers.authorization.split(" ")[1]);
-    next();
-  } catch (err) {
-    const status = 401;
-    const message = "Error: access_token is not valid";
-    res.status(status).json({ status, message });
-  }
 });
 //
 server.use((req, res, next) => {
